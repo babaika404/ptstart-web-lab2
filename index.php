5c310d641dce8d9e2b2a9d65db1a5a6150c6c3c9
babaika404 <142757851+babaika404@users.noreply.github.com>
2024-08-09 02:07:19 +0300

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 <div class="container">
     <div class="row">
         <div class="col-12 index">
-            <h1> log in </h1>
+            <h1> Posts </h1>
         <?php
         if (!isset($_COOKIE['User'])) {
         ?>
@@ -24,6 +24,17 @@
         <?php
         } else {
             // подключение к БД
+            $link = mysqli_connect('127.0.0.1', 'root', '1', 'first');
+            
+            $sql = "SELECT * FROM posts";
+            $res = mysqli_query($link, $sql);
+            if (mysqli_num_rows($res) >  0) {
+                while ($post = mysqli_fetch_array($res)) {
+                    echo "<a href='/posts.php?id=" . $post["id"] . "'>" . $post['title'] . "</a><br>";
+                }
+            } else {
+                echo "Записей пока нет";
+            }
         }
         ?>
         </div>
